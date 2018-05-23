f3058b2a742818d6c0d79d4a32fce5aff9a1a050
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2018-05-23 22:21:58 +0200

    script for FMRI data in python

--- data/fmri/generate-data.m
+++ data/fmri/generate-data.m
@@ -1,21 +1,25 @@
 ## -*- Mode: octave -*-
 
+pkg load image;
+
 X = double(imread("normal-brain.jpg")) / 256.0;
 dims = size(X);
 vec_size = size(vec(X));
-n_data = 100;
+n_data = 10;
 n_class = 2;
 
-data = zeros(n_data + 1, vec_size);
+data = zeros(n_data, vec_size);
 target = zeros(n_data, 1);
 for t=1:n_data
-  data(t, :) = vec(X + 0.1*randn(vec_size));
+  data(t, :) = vec(X + imsmooth(randn(dims),"Gaussian", 8));
 end
 
 save ("fmriTrain.dat", "data", "target");
 
-data = zeros(n_data + 1, vec_size);
+data = zeros(n_data, vec_size);
 target = zeros(n_data, 1);
-
+for t=1:n_data
+  data(t, :) = vec(X + imsmooth(randn(dims),"Gaussian", 8));
+end
 
 save ("fmriTest.dat", "data", "target")
---
+++ src/decision-problems/knn-classify-fmri.py
@@ -0,0 +1,58 @@
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
+from skimage import data
+
+original = skimage.data.imread("../../data/fmri/normal-brain.jpg")
+n_neighbors = 15
+
+# import some data to play with
+iris = datasets.load_iris()
+
+# we only take the first two features. We could avoid this ugly
+# slicing by using a two-dim dataset
+n_inputs = 2
+n_features= 16;
+X = iris.data[:, :n_inputs]
+
+y = iris.target
+
+A = np.random.rand(n_inputs, n_features)
+W = np.matmul(X, A)
+
+h = .02  # step size in the mesh
+
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+for weights in ['uniform', 'distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X, y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("3-Class classification (k = %i, weights = '%s')"
+              % (n_neighbors, weights))
+
+plt.show()
