9358d6a6fc706fd5f14eb03af16fd61fd14981d5
Christos Dimitrakakis <chrdimi@chalmers.se>
2018-05-24 17:04:06 +0200

    FMRI

---
+++ src/decision-problems/knn-classify-micromass.py
@@ -0,0 +1,31 @@
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import neighbors, datasets
+import scipy.io as sio
+
+original = sio.loadmat("../../data/micromass/data.mat")
+
+X = originaldata['X']
+
+y = originaldata['Y']
+
+### Create color maps
+### Now we can classify
+
+for weights in ['uniform', 'distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X, y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+
