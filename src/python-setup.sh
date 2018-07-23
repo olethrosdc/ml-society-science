7dc4441f3e9a56d8ebb25541ceab8f3883e99aa8
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-07-23 14:54:41 +0200

    Added chi^2 test example

--- src/python-setup.sh
+++ src/python-setup.sh
@@ -2,6 +2,8 @@ python3 ./micromass-distance.py
 python3 -m pip install --upgrade pip
 python3 -m pip install --user jupyter
 python3 -m pip install --user numpy matplotlib sklearn scipy
+python3 -m pip install --user statsmodels
 
+# exec(open("./filename").read())
 
 
