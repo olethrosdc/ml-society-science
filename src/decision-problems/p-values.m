41e49cf8fcbf08fd36fdfa904982647438af2f28
Christos Dimitrakakis <chrdimi@chalmers.se>
2018-07-31 18:18:22 +0200

    misc

--- src/decision-problems/p-values.m
+++ src/decision-problems/p-values.m
@@ -20,7 +20,7 @@ end
 figure(1)
 plot(threshold ./ [1:max_data])
 title("The rejection threshold as data increases");
-%pause
+print("p-value-example-rejection-threshold.eps")
 
 for t=1:max_data
   reject_x (t) = mean(sum(X(1:t,:)) < threshold(t));
@@ -29,33 +29,45 @@ end
 figure(2)
 plot(reject_x, ';null-distributed;', reject_y, ';other distribution;')
 title("How often we reject the null hypothesis");
-
+print("p-value-example-rejection.eps")
 
 %% However as you get more and more data, you should get more and more sure of the fact that the hypothesis is true.
 function posterior = GetPosteriorHypothesis(X)
   n_trials = columns(X);
   max_data = rows(X);
-for k=1:n_trials
-  alpha = 1;
-  beta = 1;
-  log_p_marginal = 0;
-  log_p_null = 0;
-  for t=1:max_data
-  x = X(t, k);
-  p_x = (x * alpha + (1 - x)*beta) / (alpha + beta);
-  alpha += x;
-  beta += (1 - x);
-  log_p_marginal += log(p_x);
-  log_p_null += log(1/2);
-  posterior(t,k) = exp(log_p_null - logAdd(log_p_marginal, log_p_null));
-  end
-endfor
+  for k=1:n_trials
+	alpha = 1;
+	beta = 1;
+	log_p_marginal = 0;
+	log_p_null = 0;
+	for t=1:max_data
+	  x = X(t, k);
+	  p_x = (x * alpha + (1 - x)*beta) / (alpha + beta);
+	  alpha += x;
+	  beta += (1 - x);
+	  log_p_marginal += log(p_x);
+	  log_p_null += log(1/2);
+	  posterior(t,k) = exp(log_p_null - logAdd(log_p_marginal, log_p_null));
+	endfor
+  endfor
 endfunction
 posterior_x = GetPosteriorHypothesis(X);
 posterior_y = GetPosteriorHypothesis(Y);
 figure(3);
 plot(mean(posterior_x, 2), ';null-distributed;', mean(posterior_y, 2), ';other-distributed;')
 title("Posterior probability of null hypothesis");
+print("p-value-example-posterior.eps")
 
-
+figure(4);
 %% ADD A PLOT FOR REJECTION
+
+bayes_reject_x = mean(posterior_x < 0.5, 2);
+plot(reject_x, ';null test;', bayes_reject_x, ';Bayes test;')
+title("Rejection of null hypothesis for Bernoulli(0.5)");
+print("p-value-example-null-posterior.eps")
+
+figure(5);
+bayes_reject_y = mean(posterior_y < 0.5, 2);
+plot(reject_y, ';null test;', bayes_reject_y, ';Bayes test;')
+title("Rejection of null hypothesis for Bernoulli(0.45)");
+print("p-value-example-true-posterior.eps")
