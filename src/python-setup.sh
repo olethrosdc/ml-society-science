4227283dff872b5df916ef9e199e0468d83239f7
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-08-01 21:47:12 +0200

    minor

--- src/python-setup.sh
+++ src/python-setup.sh
@@ -1,5 +1,5 @@
 python3 ./micromass-distance.py 
-python3 -m pip install --upgrade pip
+python3 -m pip install --user --upgrade pip
 python3 -m pip install --user jupyter
 python3 -m pip install --user numpy matplotlib sklearn scipy
 python3 -m pip install --user statsmodels
