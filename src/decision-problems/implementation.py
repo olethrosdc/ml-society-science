7d628e6591b5524166bd64de9e7e12e78ba490cc
Dirk Hesse <herr.dirk.hesse@gmail.com>
2018-08-27 11:05:10 +0200

    Implemented get_class_probability.

--- src/decision-problems/implementation.py
+++ src/decision-problems/implementation.py
@@ -3,7 +3,12 @@ from sklearn.linear_model import LogisticRegression
 
 class ScikitLearnModel(skeleton.Model):
     def __init__(self):
-        self.model = LogisticRegression()
+        # We initialize with very weak regularization not to throw
+        # off the testing engine. This might not be what students should/
+        # will want to do
+        self.model = LogisticRegression(C=100)
     def fit(self, X, y):
         self.model.fit(X, y)
         return self
+    def get_class_probability(self, X):
+        return self.model.predict_proba(X)
--- src/decision-problems/tests/model_tests.py
+++ src/decision-problems/tests/model_tests.py
@@ -1,16 +1,22 @@
+# Run these from src/decision-problems, using
+#
+# python -m unittest tests/model_tests.py
+
 import unittest
 from implementation import ScikitLearnModel
 
 class SklearnModelTests(unittest.TestCase):
 
     def test_fit_predict(self):
+        '''Fitting a simple two-class linear model and checking correctness.'''
         X = [[i] for i in range(10)]
         y = [0]*5 + [1]*5
         model = ScikitLearnModel()
         fitted_model = model.fit(X, y)
         probs = fitted_model.get_class_probability(X)
         for i, p in enumerate(probs):
-            if i <= 5:
-                self.assertGreater(0.5, p)
+            prob_zero, prob_one = p
+            if i < 5:
+                self.assertGreater(prob_zero, 0.5)
             else:
-                self.assertGreater(p, 0.5)
+                self.assertGreater(prob_one, 0.5)
