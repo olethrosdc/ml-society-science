d40ccae5c7e0fab24ae4d0292b37acb0b68577d8
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-08-31 18:02:03 +0200

    Added a skeleton for the banker code

---
+++ src/project-1/RandomBanker.py
@@ -0,0 +1,32 @@
+class RandomBanker:
+    
+    # Fit the model to the data.  You can use any model you like to do
+    # the fit, however you should be able to predict all class
+    # probabilities
+    def fit(self, data):
+        self.data = data
+
+    # set the interest rate
+    def set_interest_rate(self, rate):
+        self.rate = rate
+        return
+
+    # Predict the probability of failure for a specific person with data x
+    def predict_proba(self, x):
+        return 0
+
+    # THe expected utility of granting the loan or not. Here there are two actions:
+    # action = 0 do not grant the loan
+    # action = 1 grant the loan
+    #
+    # Make sure that you extract the length_of_loan from the
+    # 2nd attribute of x. Then the return if the loan is paid off to you is amount_of_loan*(1 + rate)^length_of_loan
+    # The return if the loan is not paid off is -amount_of_loan.
+    def expected_utility(self, x, action)
+
+    # Return the best action. This is normally the one that maximises expected utility.
+    # However, you are allowed to deviate from this if you can justify the reason.
+    def get_best_action(self, x):
+        return 0;
+
+
