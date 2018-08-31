9f2186784ce517402d55655a1ea707226c4d07aa
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-08-31 17:48:38 +0200

    fix

--- src/decision-problems/posterior.py
+++ src/decision-problems/posterior.py
@@ -10,7 +10,7 @@ import random
 ## - prior: belief vector so that prior[i] is the probabiltiy of mdoel i being correct
 ## - P: P[i][j] is the probability the i-th model assignsm to the j-th outcome
 ## - outcome: actual outcome
-def GetPosterior(prior, P, outcome):
+def get_posterior(prior, P, outcome):
     n_models = len(prior)
 
     ## So probability of outcome for model i is just...
@@ -21,25 +21,23 @@ def GetPosterior(prior, P, outcome):
 ## - belief: vector so that belief[i] is the probabiltiy of mdoel i being correct
 ## - P: P[i][j] is the probability the i-th model assignsm to the j-th outcome
 ## - outcome: actual outcome
-def GetMarginalPredictive(belief, P, outcome):
+def get_marginal_prediction(belief, P, outcome):
     n_models = len(belief)
 #...
     return outcome_probability
 
 ## In this function, U[action,outcome] should be the utility of the action/outcome pair
-def GetExpectedUtility(belief, P, action, U):
+def get_expected_utility(belief, P, action, U):
     n_models = len(belief)
     n_outcomes = np.shape(P)[1]
-    utility = 0
-    for outcome in range(n_outcomes):
-        p_outcome = GetMarginalPredictive(belief, P, outcome)
-        utility += U[action, outcome] * p_outcome;
+ #...
+    
     return utility
     
-def GetBestAction(belief, P, U):
+def get_best_action(belief, P, U):
     n_models = len(belief)
     n_actions = np.shape(U)[0]
-...    
+#...    
     return best_action
     
 
@@ -63,12 +61,12 @@ for t in range(T):
     for model in range(n_models):
         P[model,1] = prediction[model,t] # the table predictions give rain probabilities
         P[model,0] = 1.0 - prediction[model,t] # so no-rain probability is 1 - that.
-    probability_of_rain = GetMarginalPredictive(belief, P, 1)
+    probability_of_rain = get_marginal_prediction(belief, P, 1)
     print(probability_of_rain)
     U  = np.matrix('1 -1; 0 0')
     action = GetBestAction(belief, P, U)
     print(action, rain[t], U[action, rain[t]])
-    belief = GetPosterior(belief, P, rain[t])
+    belief = get_posterior(belief, P, rain[t])
     print(belief)
 
                 
