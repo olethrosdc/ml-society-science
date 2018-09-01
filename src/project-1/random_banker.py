3c4c3ed5c8aa3e4a0b347435f3cb42b291ce73bf
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-09-01 15:36:00 +0200

    Skeleton debugs

--- src/project-1/random_banker.py
+++ src/project-1/random_banker.py
@@ -22,11 +22,12 @@ class RandomBanker:
     # Make sure that you extract the length_of_loan from the
     # 2nd attribute of x. Then the return if the loan is paid off to you is amount_of_loan*(1 + rate)^length_of_loan
     # The return if the loan is not paid off is -amount_of_loan.
-    def expected_utility(self, x, action)
-
+    def expected_utility(self, x, action):
+        print("Not implemented")
     # Return the best action. This is normally the one that maximises expected utility.
     # However, you are allowed to deviate from this if you can justify the reason.
     def get_best_action(self, x):
-        return 0;
+        print("Not implemented")
+        
 
 
