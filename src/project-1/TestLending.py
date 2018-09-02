24f21592d7ddc2daabd52ee1862892d4e22a26e9
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-09-02 08:57:34 +0200

    test lending

---
+++ src/project-1/TestLending.py
@@ -0,0 +1,29 @@
+import pandas
+
+features = ['checking account balance', 'duration', 'credit history',
+            'purpose', 'amount', 'savings', 'employment', 'installment',
+            'marital status', 'other debtors', 'residence time',
+            'property', 'age', 'other installments', 'housing', 'credits',
+            'job', 'persons', 'phone', 'foreign']
+target = 'repaid'
+df = pandas.read_csv('../../data/credit/german.data', sep=' ',
+                     names=features+[target])
+import matplotlib.pyplot as plt
+numerical_features = ['age', 'residence time', 'installment', 'amount', 'duration', 'persons', 'credits']
+quantitative_features = list(filter(lambda x: x not in numerical_features, features))
+X = pandas.get_dummies(df, columns=quantitative_features, drop_first=True)
+encoded_features = list(filter(lambda x: x != target, X.columns))
+
+#import LogisticBanker
+#model = LogisticBanker()
+
+import random_banker
+model = random_banker.RandomBanker()
+
+model.fit(X[encoded_features], X[target])
+
+
+
+
+
+
