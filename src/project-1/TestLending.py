51ada4d672ad2803dfc3fa1179a0e78a13f6b402
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-09-02 18:02:34 +0200

    Complete test lending example

--- src/project-1/TestLending.py
+++ src/project-1/TestLending.py
@@ -14,15 +14,30 @@ quantitative_features = list(filter(lambda x: x not in numerical_features, featu
 X = pandas.get_dummies(df, columns=quantitative_features, drop_first=True)
 encoded_features = list(filter(lambda x: x != target, X.columns))
 
+
+### Setup model
 #import LogisticBanker
-#model = LogisticBanker()
-
+#decision_maker = LogisticBanker()
 import random_banker
-model = random_banker.RandomBanker()
+decision_maker = random_banker.RandomBanker()
+interest_rate = 0.05
+decision_maker.set_interest_rate(interest_rate)
+decision_maker.fit(X[encoded_features], X[target])
+n_test_examples = 100
+utiity = 0
 
-model.fit(X[encoded_features], X[target])
-
-
+for t in range(n_test_examples):
+    action = decision_maker.get_best_action(X[encoded_features].iloc[t])
+    default = X[target].iloc[t] # assume the labels are correct
+    duration = X['duration'].iloc[t]
+    amount = X['amount'].iloc[t]
+    # If we don't grant the loan then nothing happens
+    if (action==1):
+        if (default):
+            utility -= amount*(pow(1 + interest_rate) - 1)
+        else:    
+            utility += amount*(pow(1 + interest_rate) - 1)
+    
 
 
 
