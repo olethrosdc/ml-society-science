d9d44d39ece87901c1f9977da1a9f6f6b51ec32c
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-10-09 09:29:46 +0200

    Added synthetically generated data. Train/Test are in normal economic condition. Valid is during a recession.

---
+++ src/pymc/beta_bernoulli.py
@@ -0,0 +1,20 @@
+#https://docs.pymc.io/notebooks/getting_started.html
+
+import pymc3 as mc
+import numpy as np
+
+basic_model = mc.Model()
+
+data = np.array([0, 1, 0, 1])
+import matplotlib.pyplot as plt
+
+with basic_model:
+    data = np.array([1, 0, 0, 0, 1])
+    theta = mc.Beta('theta', alpha=1, beta=1)
+    y = mc.Bernoulli('y', p=theta, observed=data)
+    start = mc.find_MAP() # Find starting value by optimization
+    step = mc.NUTS(scaling=start) # Instantiate MCMC sampling algorithm
+    trace = mc.sample(2000, step, start=start, njobs=4, progressbar=True)
+
+mc.traceplot(trace)
+plt.show()
