cb32031026f86e8486831625c5bf964ad2526687
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-10-12 10:54:02 +0200

    n_users is passed as a tensor. How to use?

--- src/pymc/beta_bernoulli_clustering.py
+++ src/pymc/beta_bernoulli_clustering.py
@@ -3,6 +3,7 @@
 import pymc3 as mc
 import numpy as np
 import matplotlib.pyplot as plt
+import theano
 
 model = mc.Model()
 
@@ -10,7 +11,7 @@ n_users = 8
 n_movies = 4
 ratings = np.zeros([n_users, n_movies]);
 for u in range(n_users):
-    ## Generate data from two clusters with equal probability
+   ## Generate data from two clusters with equal probability
     if (np.random.uniform() < 0.5):
         mean = np.array([0.2, 0.3, 0.4, 0.9]);
     else:
@@ -39,10 +40,21 @@ with model:
     # We define a C*M matrix of theta parameters for this
     theta = mc.Beta('theta', alpha =1, beta = 1, shape=(n_clusters, n_movies))
 
-    
-    y = mc.Bernoulli('y', p=theta, observed=ratings)
-    
-    
+    def rating_dist(n_users, n_movies, theta, cluster, ratings):
+        log_likelihood = 0
+        print ("users: ", n_users, "movies:", n_movies)
+        for u in range(n_users):
+            for m in range(n_movies):
+                assigned_theta = theta[cluster[u], m]
+                if (ratings[u,m] == 1):
+                    log_likelihood += log(assigned_theta)
+                else:
+                    log_likelihood += log(1 - assigned_theta)
+        return log_likelihood            
+
+    rating_loglike = mc.DensityDist('rating_loglike', rating_dist, observed={'n_users':n_users, 'n_movies':n_movies, 'theta':theta, 'cluster':cluster, 'ratings':ratings})
+
+
 ## Generate samples
 #with model:
 #    step1 = mc.NUTS(vars=[zeta, theta])
