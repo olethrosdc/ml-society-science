0f9b6a70e35f440c21d76a7f46dcd850f44b1044
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-10-28 06:57:57 +0100

    Code for project

--- src/project-2/random_recommender.py
+++ src/project-2/random_recommender.py
@@ -2,11 +2,28 @@
 # A random recommender
 #
 #
-# For this recommendation system, assume a scenario where
+# This is a medical scenario with historical data. 
 #
+# General functions
+#
+# - set_reward
 # 
+# There is a set of functions for dealing with historical data:
+#
+# - fit_data
+# - fit_treatment_outcome
+# - estimate_utiltiy
+#
+# There is a set of functions for online decision making
+#
+# - predict_proba
+# - recommend
+# - observe
+
+
 class RandomRecommender:
 
+    ##################################
     # Fit a model from patient data.
     #
     # This will generally speaking be an
@@ -17,16 +34,21 @@ class RandomRecommender:
     def fit_data(self, data):
         return None
 
+    # Set the reward function r(a, y)
+    def set_reward(self, reward):
+        self.reward = reward
+        
     ## Fit a model from patient data, actions and their effects
     ## Patient data.
     def fit_treatment_outcome(self, data, actions, outcome):
         return None
 
-    # Fit a model from historical data, given treatments, effects, as well as the treatment policy
-    def fit_policy_outcome(self, data, actions, outcomes, policy):
+    ## Estimate the utility from historical data.
+    ## If the policy is None, return use the historical policy
+    def estimate_utility(self, data, actions, outcome, U, policy=None):
         return None
 
-    # Return a distribution of effects for a given person's data and a specific treatmentsssssssssssssssssssssssssssssss
+    # Return a distribution of effects for a given person's data and a specific treatment
     def predict_proba(self, data, treatment):
         return None
     
