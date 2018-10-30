297ade3412e7fe3b6238515ca8ba3bafc8f3d001
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2018-10-30 20:51:28 +0100

    Added modified recommender

--- data/COMPAS/README.md
+++
@@ -1,4 +0,0 @@
-# COMPAS dataset
-
-- Background story: https://www.propublica.org/article/machine-bias-risk-assessments-in-criminal-sentencing
-- Data source: https://github.com/propublica/compas-analysis
--- src/project-2/random_recommender.py
+++ src/project-2/random_recommender.py
@@ -1,35 +1,78 @@
 # -*- Mode: python -*-
-# A random recommender
+# A simple reference recommender
+#
+#
+# This is a medical scenario with historical data. 
+#
+# General functions
+#
+# - set_reward
+# 
+# There is a set of functions for dealing with historical data:
+#
+# - fit_data
+# - fit_treatment_outcome
+# - estimate_utiltiy
+#
+# There is a set of functions for online decision making
+#
+# - predict_proba
+# - recommend
+# - observe
+
+from sklearn import linear_model
+import numpy as np
 
 class RandomRecommender:
 
+    #################################
+    # Initialise
+    #
+
+    def __init__(self):
+        self.reward = self._default_reward
+
+    ## By default, the reward is just equal to the outcome, as the actions play no role
+    def _default_reward(self, action, outcome):
+        return outcome
+
+    # Set the reward function r(a, y)
+    def set_reward(self, reward):
+        self.reward = reward
+    
+    ##################################
     # Fit a model from patient data.
+    #
     # This will generally speaking be an
     # unsupervised model. Anything from a Gaussian mixture model to a
     # neural network is a valid choice.  However, you can give special
     # meaning to different parts of the data, and use a supervised
     # model instead.
     def fit_data(self, data):
+        print("Preprocessing data")
         return None
+
 
     ## Fit a model from patient data, actions and their effects
-    ## Patient data.
+    ## Here we assume that the outcome is a direct function of data and actions
     def fit_treatment_outcome(self, data, actions, outcome):
+        print("Fitting treatment outcomes")
         return None
 
-    # Fit a model from historical data, given treatments, effects, as well as the treatment policy
-    def fit_policy_outcome(self, data, actions, outcomes, policy):
+    ## Estimate the utility of a specific policy from historical data.
+    ## If the policy is None, return use the historical policy
+    def estimate_utility(self, data, actions, outcome, policy=None):
         return None
 
-    # Return a distribution of effects for a given person's data and a specific treatmentsssssssssssssssssssssssssssssss
+    # Return a distribution of effects for a given person's data and a specific treatment
     def predict_proba(self, data, treatment):
         return None
     
     # Return recommendations for a specific user data
     def recommend(self, user):
+        print("Recommending")
         return None
 
     # Observe the effect of an action
-    def observe(self, user, action, reward):
+    def observe(self, user, action, outcome):
         return None
-
