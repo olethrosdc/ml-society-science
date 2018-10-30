06e969a470d531d1ac1d5e90808ab5b09282c04e
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2018-10-30 20:55:04 +0100

    Updated project 2

--- src/project-2/TestRecommender.py
+++ src/project-2/TestRecommender.py
@@ -1,5 +1,17 @@
 import numpy as np
 import pandas
+def default_reward_function(action, outcome):
+    return outcome
+
+def test_policy(generator, policy, reward_function, T):
+    policy.set_reward(reward_function)
+    u = 0
+    for t in range(T):
+        x = generator.generate_features()
+        a = policy.recommend(x)
+        y = generator.generate_outcome(x, a)
+        u += reward_function(a, y)
+    return u
 
 features = pandas.read_csv('data/medical/historical_X.dat', header=None, sep=" ").values
 actions = pandas.read_csv('data/medical/historical_A.dat', header=None, sep=" ").values
@@ -7,17 +19,8 @@ outcome = pandas.read_csv('data/medical/historical_Y.dat', header=None, sep=" ")
 observations = features[:, :8]
 labels = features[:,128] + features[:,129]*2
 
-def default_reward_function(action, outcome):
-    return outcome
-
-def test_policy(generator, policy, reward_function, T):
-    generator.set_reward(reward_function)
-    for t in range(T):
-        x = generator.generate_features
-    
-        
 import reference_recommender
-policy_factory = reference_recommender.Reference_Recommender
+policy_factory = reference_recommender.ReferenceRecommender
 policy = policy_factory()
 
 import data_generation
--- src/project-2/data_generation.py
+++ src/project-2/data_generation.py
@@ -10,7 +10,7 @@ class DataGenerator:
         self.V = parameters['V'] # the treatment effect matrix
         self.W = parameters['W'] # the feature matrix
 
-    def GenerateFeatures(self):
+    def generate_features(self):
         T = np.random.normal(size=(1, self.W.shape[0]))
         X = np.zeros([1,130])
         X[0,0:128] = 1*(np.matmul(T, self.W) < 0)
@@ -26,14 +26,21 @@ class DataGenerator:
             X[0,129] = 1*(np.random.uniform() < 0.2 * X[0,1]*X[0,3] + 0.2 * X[0,4] - 0.1 * X[0,9])
         return X
 
-    def GenerateDefaultAction(self, X):
+    def generate_default_action(self, X):
         A = 1*(np.random.uniform() < X[0,128] * 0.4  + X[0,129] * 0.5);
         return A
 
-    def GenerateOutcome(self, A):
+    def generate_outcome(self, X, A):
         Y = 1*(self.V[A] * X > 0);
         return Y
 
+    def generate_data(self, n_data):
+        X = np.zeros([n_data,130])
+        for t in range(n_data):
+            X[t] = generate_features()
+            A[t] = generate_default_action(X[t])
+            Y[t] = generate_outcome(X[t], A[t])
+            
 if __name__ == '__main__':
     n_data = 1000
 
