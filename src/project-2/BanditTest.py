991e0c8ca9da11da2f00c6c183728ca3f883610d
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2018-11-08 12:43:05 +0100

    Missing file

--- src/project-2/BanditTest.py
+++ src/project-2/BanditTest.py
@@ -19,10 +19,11 @@ def test_policy(generator, policy, reward_function, T):
     print("Total reward: ", u)
     return r
 
-n_actions = 2
+n_actions = 16
 n_outcomes = 2
 import average_bandit
-average_policy = average_bandit.AverageBandit(n_actions, n_outcomes, 1)
+average_policy = average_bandit.AverageBandit(n_actions, n_outcomes, 0)
+average_policy_2 = average_bandit.AverageBandit(n_actions, n_outcomes, 10)
 import softmax_bandit
 softmax_policy = softmax_bandit.SoftmaxBandit(n_actions, n_outcomes, 1)
 import thompson_bandit
@@ -34,9 +35,10 @@ T = 10000
 import bernoulli_bandit
 generator = bernoulli_bandit.BernoulliBandit(n_actions)
 average_result = test_policy(generator, average_policy, simple_reward_function, T)
+#average_result2 = test_policy(generator, average_policy_2, simple_reward_function, T)
 softmax_result = test_policy(generator, softmax_policy, simple_reward_function, T)
 thompson_result = test_policy(generator, thompson_policy, simple_reward_function, T)
 results = pandas.DataFrame({'Thompson': thompson_result, 'Softmax': softmax_result, 'Linear': average_result})
-results = results.cumsum()
+
 results.rolling(window=int(np.ceil(np.sqrt(T)))).sum().plot()
 plt.show()
