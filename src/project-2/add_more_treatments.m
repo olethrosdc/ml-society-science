7e905201029a9bbc172a46ff091937cc6dd3286c
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2018-11-11 15:51:48 +0100

    Adding more treatments

--- src/project-2/add_more_treatments.m
+++ src/project-2/add_more_treatments.m
@@ -13,10 +13,23 @@ diff = mean(X_pos - mean(X_neg));
 V_average = diff;
 
 V = [V; V_average];
-V_eye = eye(130);
+V_eye = eye(130) + repmat(V(1,:), 130,1);
 for k =3:128
   V = [V; V_eye(k,:)];
+  V(k,k) *= randn;
 end
 
 printf("Average treatment effect\n")
-mean(V * X_pos', 2)
+effect = mean(V * X_pos', 2);
+plot(effect)
+
+best_effect = zeros(rows(X_pos), 1);
+for t=1:rows(X_pos)
+  best_effect(t)= max(V * X(1, :)');
+end
+
+printf("- Best uniform treatment: %f\n- Best custom treatment: %f\n",
+	   max(effect),
+	   mean(best_effect))
+
+save("-mat", "big_generating_matrices.mat", "V", "W")
