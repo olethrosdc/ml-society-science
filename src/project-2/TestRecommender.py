6b1be3ccf3bdfea473903528eef8758cf6edcfbe
Christos Dimitrakakis <chrdimi@chalmers.se>
2018-11-14 10:36:09 +0100

    A bit more text

--- src/project-2/TestRecommender.py
+++ src/project-2/TestRecommender.py
@@ -4,6 +4,7 @@ def default_reward_function(action, outcome):
     return outcome
 
 def test_policy(generator, policy, reward_function, T):
+    print("Testing for ", T, "steps")
     policy.set_reward(reward_function)
     u = 0
     for t in range(T):
@@ -30,7 +31,7 @@ policy_factory = random_recommender.RandomRecommender
 #policy_factory = reference_recommender.RandomRecommender
 
 ## First test with the same number of treatments
-print("Testing with only two treatments")
+print("---- Testing with only two treatments ----")
 
 print("Setting up simulator")
 generator = data_generation.DataGenerator(matrices="./generating_matrices.mat")
@@ -41,16 +42,16 @@ print("Fitting historical data to the policy")
 policy.fit_treatment_outcome(features, actions, outcome)
 ## Run an online test with a small number of actions
 print("Running an online test")
-n_tests = 100
+n_tests = 1000
 result = test_policy(generator, policy, default_reward_function, n_tests)
 print("Total reward:", result)
 print("Final analysis of results")
 policy.final_analysis()
 
 ## First test with the same number of treatments
-print("Testing with an additional experimental treatment and 126 other treatments")
+print("--- Testing with an additional experimental treatment and 126 gene silencing treatments ---")
 print("Setting up simulator")
-generator = data_generation.DataGenerator(matrices="./generating_matrices.mat")
+generator = data_generation.DataGenerator(matrices="./big_generating_matrices.mat")
 print("Setting up policy")
 policy = policy_factory(generator.get_n_actions(), generator.get_n_outcomes())
 ## Fit the policy on historical data first
@@ -58,7 +59,7 @@ print("Fitting historical data to the policy")
 policy.fit_treatment_outcome(features, actions, outcome)
 ## Run an online test with a small number of actions
 print("Running an online test")
-n_tests = 100
+n_tests = 1000
 result = test_policy(generator, policy, default_reward_function, n_tests)
 print("Total reward:", result)
 print("Final analysis of results")
