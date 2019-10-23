732cff071d8482d984026a319dcdf706214df2f0
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2019-10-23 14:54:18 +0200

    oops

--- src/recomm/similarity_recommend_skeleton.py
+++ src/recomm/similarity_recommend_skeleton.py
@@ -4,19 +4,10 @@ import numpy as np
 
 ## Calculate the distance between two people
 def distance(x, y):
-    d = 0
-    n_movies = len(x)
-    for i in range(n_movies):
-        d += abs(x[i] - y[i])
-    return d
 
 def get_similarity(data, u):
     n_users = data.shape[0]
     similarity = np.zeros(n_users)
-    for v in range(n_users):
-        if (v != u):
-            similarity[v] = np.exp(-distance(data[u], data[v]))
-    return similarity / np.sum(similarity)
         
 def generate_random_data(n_users, n_movies, gamma):
     data = np.zeros([n_users, n_movies])
@@ -33,11 +24,6 @@ def infer_ratings(data, linkage):
     n_users = data.shape[0]
     n_movies = data.shape[1]
     inferred_ratings = data
-    for u in range(n_users):
-        w = get_similarity(data, u)
-        for m in range(n_movies):
-            if (data[u, m] == 0):
-                inferred_ratings[u, m] = data[:,m].dot(w)
     return inferred_ratings
 
 
