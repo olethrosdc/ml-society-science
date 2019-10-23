501529fbb288fec2bdf364149b174925b2ae7f52
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2019-10-23 15:24:32 +0200

    Skeleton update

--- src/recomm/similarity_recommend_skeleton.py
+++ src/recomm/similarity_recommend_skeleton.py
@@ -6,10 +6,29 @@ import numpy as np
 ## (helper function, if necessary)
 def distance(x, y):
 
-## Get the similarity between two people
+## Get the similarity between two people.
+## I recommend that this actually retunrs a similarity vector that sums to 1, and has a zero value for the user himself
 def get_similarity(data, u):
     n_users = data.shape[0]
-    similarity = np.zeros(n_users)
+    similarity = np.ones(n_users) / (n_users - 1)
+    similarity[u] = 0
+    
+
+
+## data[u,m] = 0 if somebody hasn't watched a movie
+def infer_ratings(data):
+    n_users = data.shape[0]
+    n_movies = data.shape[1]
+    inferred_ratings = data
+    ## Do a loop wher you fill in values for each user and movie
+    for u in range(n_users):
+        
+        ## calculate neighbour weights
+        ## Get prediction for movies
+    return inferred_ratings
+
+
+    
         
 def generate_random_data(n_users, n_movies, gamma):
     data = np.zeros([n_users, n_movies])
@@ -20,14 +39,3 @@ def generate_random_data(n_users, n_movies, gamma):
         for m in ratings:
             data[u,m] = 1 + np.random.choice(5,1, p=rating_dist)
     return data
-
-
-## data[u,m] = 0 if somebody hasn't watched a movie
-def infer_ratings(data):
-    n_users = data.shape[0]
-    n_movies = data.shape[1]
-    inferred_ratings = data
-    return inferred_ratings
-
-
-    
