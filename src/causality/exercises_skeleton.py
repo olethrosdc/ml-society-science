1cbbe3d979599fde79b069c35fc28f5b079fa9ca
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2019-10-31 10:54:19 +0100

    data generation

--- src/causality/exercises_skeleton.py
+++ src/causality/exercises_skeleton.py
@@ -13,3 +13,27 @@ for t in range(n_data):
     a[t] = policy.get_action()
     x[t] = model.get_response(a[t])
     
+
+y = np.zeros(n_data)
+for t in range(n_data):
+    y[t] = x[t] - a[t]
+
+print(np.mean(y))
+
+### Get an estimate for theta for the model where the response is one of two different normal distributions, whose mean arbitrarily depends on the action
+theta = 0.1*np.random.normal(size=2)
+model = StandardModel(theta)
+policy = BasicPolicy(0.1)
+n_samples = 10
+n_actions = policy.get_n_actions()
+a = np.empty(n_samples, dtype=int)
+y = np.zeros(n_samples)
+hat_theta = np.zeros(2) # use this to estimate the model
+hat_pi = np.zeros(2) # use this to estimate the policy
+counts = np.zeros(2) # this is needed for getting the right 
+
+## This corresponds to estimating the historical policy, the model parameters and the expected utility of that policy, with the only difference that the data here is generated online. So, the code could be used to estimate the utility of a new policy, if you had access to the model.
+## Generate data
+for t in range(n_samples):
+    a[t] = int(policy.get_action()) 
+    y[t] = model.get_response(a[t])
