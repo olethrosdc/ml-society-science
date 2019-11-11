772161f3d9c346a1e2bc2d7636dfc8a29f55cee1
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2019-11-11 07:20:18 +0100

    misc

--- src/python-setup.sh
+++ src/python-setup.sh
@@ -4,6 +4,7 @@ python3 -m pip install --user numpy matplotlib sklearn scipy
 python3 -m pip install --user statsmodels
 python3 -m pip install --user pytest
 python3 -m pip install --user pymc
+python3 -m pip install --user pystan
 python3 -m pip install --user pandas
 python3 -m pip install --user keras
 python3 -m pip install --user spark pyspark
