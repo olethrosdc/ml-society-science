52706e1da6de3df4e4277b80942ab3b38e80cce1
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2020-10-28 09:58:48 +0100

    missing diagram

--- src/recomm/similarity_recommend_skeleton.py
+++ src/recomm/similarity_recommend_skeleton.py
@@ -5,11 +5,17 @@ import numpy as np
 ## Calculate the distance between two people
 ## (helper function, if necessary)
 def distance(x, y):
-    return
+    return np.sum((x-y)**(x-y))
 
 ## Get the similarity between two people.
 ## I recommend that this actually retunrs a similarity vector that sums to 1, and has a zero value for the user himself
-def get_similarity(data, u, m):
+def get_similarity(data, u):
+    n_users = data.shape[0]
+    n_movies = data.shape[1]
+    similarity = np.zeros(n_users)
+    for k in range(n_users):
+        if (u!=k):
+            similarity[k] = np.exp(-0.01*distance(data[u], data[k]))
     return similarity / np.sum(similarity)
 
 ## data[u,m] = 0 if somebody hasn't watched a movie
@@ -17,9 +23,21 @@ def infer_ratings(data):
     n_users = data.shape[0]
     n_movies = data.shape[1]
     inferred_ratings = data.copy()
-    ## Do a loop wher you fill in values for each user and movie
-    return inferred_ratings
+    for u in range(n_users):
+        raw_similarity = get_similarity(data, u)
+        for m in range(n_movies):
+            ## Only calculate for movies we haven't rated
+            if (data[u,m]==0):
+                similarity_m = np.zeros(n_users)
+                ## Ignore 0s
+                for j in range(n_users):
+                    if (data[j,m] > 0):
+                        similarity_m = raw_similarity
+                ##similarity_m(data==0) = 0 ?
+                similarity_m /= np.sum(similarity_m)
 
+                inferred_ratings[u,m] = similarity_m.dot(data[:,m])
+    return inferred_ratings
 
     
         
