dcb786488e25d2fd6593eea12a8c00458a2e1f4a
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2020-11-04 10:46:28 +0100

    Exercise 15

--- src/causality/exercises_skeleton.py
+++ src/causality/exercises_skeleton.py
@@ -21,8 +21,9 @@ for t in range(n_data):
 print("Average outcome:", np.mean(y))
 
 ### Get an estimate for theta for the model where the response is one of two different normal distributions, whose mean arbitrarily depends on the action
-theta = 0.1*np.random.normal(size=2)
-model = StandardModel(theta)
+#theta = 0.1*np.random.normal(size=2)
+theta = 0.1
+model = BasicModel(theta)
 policy = BasicPolicy(0.1)
 n_samples = 1000
 n_actions = policy.get_n_actions()
@@ -31,7 +32,7 @@ y = np.zeros(n_samples)
 #hat_theta = np.zeros(2) # use this to estimate the model
 hat_theta = 0
 hat_pi = np.zeros(2) # use this to estimate the policy
-counts = np.zeros(2) # this is needed for getting the right 
+counts = np.zeros(2) # this is needed for getting the right
 hat_U = 0
 
 
@@ -43,14 +44,15 @@ for t in range(n_samples):
 for t in range(n_samples):
     hat_U += y[t]
     hat_theta += y[t] - a[t]
-
+    counts[a[t]] += 1
+    
 hat_U /= n_samples
-#hat_theta /= counts
-
+hat_theta /= n_samples
+hat_pol = counts / counts.sum()
 print("Actual parameter: ", theta)
 print("Estimated parameter:", hat_theta)
 print("Estimated utility:", hat_U)
-
+print("Estimated policy:", hat_pol)
 
 ## Exercise 1: Calculate the actual value of the improved policy by generating new data from that policy
 
