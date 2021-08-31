986fd207e9051d72669f936ac6fa391dc9d263a3
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2021-08-31 14:11:34 +0200

    Minor

--- src/decision-problems/optimal_action_infinite.py
+++ src/decision-problems/optimal_action_infinite.py
@@ -1,22 +1,3 @@
 import tensorflow as tf
 import numpy as np
 
-## Toy example ##
-# Create an optimizer with the desired parameters.
-opt = tf.keras.optimizers.SGD(learning_rate=0.1)
-var1 = tf.Variable(1.0);
-var2 = tf.Variable(1.0);
-# `loss` is a callable that takes no argument and returns the value
-# to minimize.
-loss = lambda: 3 * var1 * var1 + 2 * var2 * var2
-
-# In eager mode, simply call minimize to update the list of variables.
-n_iter=10
-for i in range(n_iter):
-    opt.minimize(loss, var_list=[var1, var2])
-    print([var1.numpy(), var2.numpy()])
-    
-
-## Example with a more complex utility function
-
-
