b06eafe6c49263e906fe71a15175c4056d3682fa
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2021-09-29 09:21:09 +0200

    Moved

--- src/privacy/randomised_response.py
+++ src/privacy/randomised_response.py
@@ -17,14 +17,12 @@ print("The empirical ratio is ", empirical_ratio)
 
 #### Random-Resposne mechanism for local DP
 ## If $\epsilon = ln (1-p)/p$ then we need to set $p = 1 / (1 + e^\epsilon)$
-p_flip = 1 / (1 + numpy.exp(epsilon))
-dp_data = numpy.zeros(n_people)
-for i in range(n_people):
-    if (numpy.random.binomial(
-    dp_data[i] 
+def RandomisedResponse(x, epsilon):
+    
+
 # Calculate the average
-local_average = numpy.average(corrupted_data)
-print("The average salary computed with local DP + Laplace is ", local_average)
+local_average = numpy.average(private_data)
+print("The average salary computed with local DP + randomised response ", local_average)
 
 #### Laplace mechanism for centralised DP
 #
