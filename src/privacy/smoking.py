eb8adcb54ee8f1e085fdc3b59985c9128e3e5196
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2021-09-29 11:00:00 +0200

    Comments

--- src/privacy/smoking.py
+++ src/privacy/smoking.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ## We want to calculate the share of people smoking
+## Repeat the experiment for varying people and epsilon
 n_people = 100 # The number of people participating
 epsilon = 0.1 # the amount of privacy we can lose
 
-# Assume the probability of smoking is fixed
+# Assume the probability of smoking is fixed.
 true_ratio = 0.1
+# Now we generate some data.
 data=np.random.choice(2, size=n_people, p=[1 - true_ratio, true_ratio])
 
 # Calculate the average salary
@@ -16,9 +18,12 @@ print("The empirical ratio is ", empirical_ratio)
 
 import randomised_response
 
+# Implement the randomised response mechanism
+# so that the flipping probability gives you exactly
+# epsilon differential privacy
 private_data = randomised_response.rp_binary(data, epsilon)
 
-# Calculate the average
+# Calculate the average on the private version of the data
 local_average = np.average(private_data)
 print("The average probability of smoking computed with local DP + randomised response ", local_average)
 
