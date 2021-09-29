8f11381bc7d2c81b248f1301c36f117cdb4e8962
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2021-09-29 10:09:43 +0200

    Skeleton

--- src/privacy/salary-plot-skeleton.py
+++ src/privacy/salary-plot-skeleton.py
@@ -16,64 +16,6 @@ average_salary = numpy.average(data)
 print("The actual average salary is ", average_salary)
 
 
-#### Laplace mechanism for local DP
-#
-# We need the sensitivity of individual data points. Since an
-# individual's data can vary at most by max_salary, we have:
-def LocalDPLaplace(data, epsilon, max_salary, n_people):
-    noise = numpy.random.laplace(scale = max_salary/epsilon, size = n_people)
-    local_average = numpy.average(data + noise)
-    return local_average
-
-#### Laplace mechanism for centralised DP
-#
-# We calculate the average, so if an individual's data changes by max_salary, the average
-# changes by max_salary / n. So:
-def CentralDPLaplace(data, epsilon, max_salary, n_people):
-    sensitivity = max_salary / n_people
-    noise = numpy.random.laplace(scale = sensitivity/epsilon, size = 1)
-    central_average = numpy.average(data) + noise
-    return central_average
-
-#### Exponential mechanism for local DP
-#
-# Here each person uses the exponential mechanism to generate a
-# response from their true data.
-#
-# The utility of different responses for a salary response
-def LocalDPExponential(data, epsilon, max_salary, n_people):
-    #utility = - abs(salary - response)^2?
-    sensitivity = max_salary**2
-    noise= numpy.random.normal(scale = sensitivity/epsilon, size = n_people)
-    local_average = numpy.average(data + noise)
-    return local_average
-
-
-#### Exponential mechanism for central DP
-#
-# Here exponential mechanism to generate a
-# response from their true data. 
-def CentralDPExponential(data, epsilon, max_salary, n_people):
-    sensitivity = (max_salary/n_people)**2  # not sure about this
-    noise= numpy.random.normal(scale = sensitivity/epsilon, size = 1)
-    local_average = numpy.average(data) + noise
-    return central_average
-
-### Exponential mechanism for local DP
-#
-# Here the exponential mechanism takes the utility as an input and a
-# finite number of responses
-def GenericExponentialMechanism(data, epsilon, utility, n_responses):
-    L = utility.sensitivity()
-    p = numpy.zeros(n_responses)
-    for a in range(n_responses):
-        p[a] = utility.calculate(answer, data)
-
-    p /= p.sum()
-
-    answer = numpy.random.choice(a = range(n_responses), p = p)
-    
-    return answer
 
 
 
--- src/privacy/smoking.py
+++ src/privacy/smoking.py
@@ -1,18 +1,16 @@
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 ## We want to calculate the share of people smoking
 n_people = 100 # The number of people participating
 epsilon = 0.1 # the amount of privacy we can lose
-
+max_salary=10000
 # Assume the probability of smoking is fixed
 true_ratio = 0.1
-data=numpy.zeros(n_people)
-for i in range(n_people):
-    data[i] = min(data[i], max_salary)
+data=np.random.coice(2, size=n_people, p=[1 - true_ratio, true_ratio])
 
 # Calculate the average salary
-empirical_ratio = numpy.average(data)
+empirical_ratio = np.average(data)
 print("The empirical ratio is ", empirical_ratio)
 
 
@@ -21,10 +19,7 @@ import randomised_response
 private_data = randomised_response.rp_binary(data, epsilon)
 
 # Calculate the average
-local_average = numpy.average(private_data)
+local_average = np.average(private_data)
 print("The average salary computed with local DP + randomised response ", local_average)
-
-central_average = numpy.average(data + central_noise)
-print("The average salary computed with central DP + Laplace is ", central_average)
 
 
