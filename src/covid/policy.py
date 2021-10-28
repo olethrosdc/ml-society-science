83c2d92ac48bbba8b27221b4581001e81c9f6eb3
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2021-10-28 19:29:45 +0200

    Testing

--- src/covid/policy.py
+++ src/covid/policy.py
@@ -13,8 +13,9 @@ class Policy:
     def __init__(self, n_actions, action_set):
         """ Initialise.
 
-        :param int n_actions: the number of actions
-        :param list action_set: the set of actions
+        Args:
+        n_actions (int): the number of actions
+        action_set (list): the set of actions
         """
         self.n_actions = n_actions
         self.action_set = action_set
@@ -24,23 +25,46 @@ class Policy:
     def observe(self, features, action, outcomes):
         """ Observe features, actions and outcomes.
 
-        :param array features
-        :param array actions
-        :param array outcomes
+        Args:
+        features (t*|X| array)
+        actions (t*|A| array)
+        outcomes (t*|Y| array)
+        
         """
         pass
-    def get_utility(self, features, treatment, outcome):
-        """ Obtain the empirical utility of the policy on a set of one or more people. """
+    def get_utility(self, features, action, outcome):
+        """ Obtain the empirical utility of the policy on a set of one or more people. 
+
+        If there are t individuals with x features, and the action
+        
+        Args:
+        features (t*|X| array)
+        actions (t*|A| array)
+        outcomes (t*|Y| array)
+
+        Returns:
+        Empirical utility of the policy on this data.
+
+        """
 
         return 0
     def get_action(self, features):
-        """ Get actions fro one or more people. """
+        """ Get actions for one or more people. 
+
+        Args: 
+        features (t*|X| array)
+
+        Returns: 
+        actions (t*|A| array)
+        """
         return 0
 
-## This is a purely random policy!
 class RandomPolicy(Policy):
-    ## Here the utiliy is defined in terms of the outcomes obtained only, ignoring both the treatment and the previous condition.
-    def get_utility(self, features, treatment, outcome):
+    """ This is a purely random policy!"""
+
+    def get_utility(self, features, action, outcome):
+        """Here the utiliy is defined in terms of the outcomes obtained only, ignoring both the treatment and the previous condition.
+        """
         actions = self.get_action(features)
         utility = 0
         utility -= 0.2 * sum(outcome[:,symptom_names['Covid-Positive']])
@@ -53,10 +77,25 @@ class RandomPolicy(Policy):
         utility -= 1.0 * sum(outcome[:,symptom_names['Blood-Clots']])
         utility -= 100.0 * sum(outcome[:,symptom_names['Death']])
         return utility
-    def get_action(self, features):
-        if (features.ndim > 1):
-            return np.random.choice(self.actions_set, features.shape[0])
-        else:
-            return np.random.choice(self.action_set)
-
     
+    def get_action(self, features):
+        """Get a completely random set of actions, but only one for each individual.
+
+        If there is more than one individual, feature has dimensions t*x matrix, otherwise it is an x-size array.
+        
+        It assumes a finite set of actions.
+
+        Returns:
+        A t*|A| array of actions
+        """
+
+        n_people = features.shape[0]
+        actions = np.zeros([n_people, self.n_actions])
+        for t in range(features.shape[0]):
+            action = np.random.choice(self.action_set)
+            if (action >= 0):
+                actions[t,action] = 1
+            
+        return actions
+
+
--- src/covid/simulator.py
+++ src/covid/simulator.py
@@ -28,7 +28,8 @@ class Person:
                 self.comorbidities[i] = 1
             else:
                 self.comorbidities[i] = 0
-        self.symptom_baseline = np.array([pop.historical_prevalence, pop.prevalence, 0.3, 0.2, 0.05, 0.2, 0.02, 0.05, 0.2, 0.1]);
+
+        self.symptom_baseline = np.array([pop.historical_prevalence, pop.prevalence, 0.01, 0.05, 0.05, 0.01, 0.02, 0.001, 0.001, 0.001]);
         self.symptom_baseline = np.array(np.matrix(self.genes) * pop.G).flatten() * self.symptom_baseline
         self.symptom_baseline[0] = pop.historical_prevalence;
         self.symptom_baseline[1] = pop.prevalence;
@@ -37,25 +38,35 @@ class Person:
         else:
             self.symptom_baseline[7] += 0.01
             self.symptom_baseline[9] += 0.01
-        ## previous infection
 
         # Initially no symptoms apart from Covid+/CovidPre
         self.symptoms = [0] * pop.n_symptoms
         if (np.random.uniform() <= self.symptom_baseline[0]):
             self.symptoms[0] = 1
+
+        # increase symptom probabilities for symptoms when covid+
         if (np.random.uniform() <= self.symptom_baseline[1]):
             self.symptoms[1] = 1
+            self.symptom_baseline = np.array([pop.historical_prevalence, pop.prevalence, 0.3, 0.2, 0.05, 0.2, 0.02, 0.05, 0.2, 0.1]);
+
         self.vaccines=[0] * pop.n_vaccines
 
     # use vaccine = -1 if no vaccine is given
-    def vaccinate(self, vaccine, pop):
+    def vaccinate(self, vaccine_array, pop):
         ## Vaccinated
-        print(vaccine, pop.n_vaccines)
-        if (vaccine >= 0):
-            self.vaccines[vaccine] = 1
+        print("V:", vaccine_array, pop.n_vaccines)
+        if (sum(vaccine_array) >=0 ):
+            vaccinated = True
+        else:
+            vaccinated = False
+            
+        if (vaccinated):
+            vaccine = np.argmax(vaccine_array)
+            self.vaccines = vaccine_array
             self.symptom_baseline[1] *= pop.baseline_efficacy[vaccine]
 
-        if (vaccine >= 0 and self.symptoms[1]==1):
+
+        if (vaccinated and self.symptoms[1]==1):
             self.symptom_baseline[[2,3,4,6]] *= pop.mild_efficacy[vaccine]
             self.symptom_baseline[[5,7,8]] *= pop.critical_efficacy[vaccine]
             self.symptom_baseline[9] *= pop.death_efficacy[vaccine]
@@ -76,9 +87,9 @@ class Person:
                 self.symptom_baseline[3]+=0.2
                 self.symptom_baseline[4]+=0.1
 
-        # 10% long covid sufferers
-        if (self.symptoms[0]==1 and self.symptoms[1]==0): 
-            self.symptom_baseline *= 2
+        # model long covid sufferers by increasing the chances of various symptoms slightly
+        if (self.symptoms[0]==1 and self.symptoms[1]==0):
+            self.symptom_baseline += np.array([0, 0, 0.06, 0.04, 0.01, 0.04, 0.004, 0.01, 0.04, 0.01]);
 
         # genetic factors
         self.symptom_baseline = np.array(np.matrix(self.genes) * pop.G).flatten() * self.symptom_baseline
@@ -118,26 +129,54 @@ class Population:
     ## X: characteristics before treatment, including whether or not they were vaccinated 
     ## The generated population may already be vaccinated.
     def generate(self, n_individuals):
+        """Generate a population. 
+
+        Call this function before anything else is done. 
+        Calling this function again generates a completely new population sample, purging the previous one from memory.
+
+        :param int n_individuals: the number of individuals to generate
+
+        """
         X = np.zeros([n_individuals, 3 + self.n_genes + self.n_comorbidities + self.n_vaccines + self.n_symptoms])
         Y = np.zeros([n_individuals, self.n_treatments, self.n_symptoms])
         self.persons = []
         for t in range(n_individuals):
             person = Person(self)
             vaccine = np.random.choice(4, p=self.vaccination_rate) - 1
-            person.vaccinate(vaccine, self)
+            vaccine_array = np.zeros(self.n_vaccines)
+            print(":", vaccine_array)
+            if (vaccine>=0):
+                vaccine_array[vaccine] = 1
+            person.vaccinate(vaccine_array, self)
             self.persons.append(person)
             x_t = np.concatenate([person.symptoms, [person.age, person.gender, person.income], person.genes, person.comorbidities, person.vaccines])
             X[t,:] = x_t
         self.X = X
         return X
 
-    def vaccinate(self, person_index, vaccine):
-        self.persons[person_index].vaccinate(vaccine, self)
+    def vaccinate(self, person_index, vaccine_array):
+        """ Give a vaccine to a specific person.
+
+
+        Args:
+        person_index (int), index of a person in the population
+        vaccine_array (|A| array), array indicating which vaccines are given.
+        
+        Notes:
+        Currently only one vaccine dose is implemented, but in the future multiple doses may be modelled.
+        """
+        self.persons[person_index].vaccinate(vaccine_array, self)
         return self.persons[person_index].symptoms
 
     def treat(self, person_index, treatment):
-        treatments = np.zeros(self.n_treatments)
-        treatments[treatment] = 1
+        """ Treat a patient.
+
+        Args:
+        person_index (int), index of a person in the population
+        treatment_array (|A| array), array indicating which treatment are to be given.
+
+        """
+
         r = np.array(treatment * self.A).flatten()
         result = np.zeros(self.n_symptoms)
         for k in range(self.n_symptoms):
