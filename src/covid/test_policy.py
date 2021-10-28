48e5eff1a6f7c94c28c2c4931ac9a9a53e92a735
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-10-28 22:01:38 +0200

    Typo in for loop

--- src/covid/test_policy.py
+++ src/covid/test_policy.py
@@ -8,13 +8,14 @@ import simulator
 n_genes = 128
 n_vaccines = 3
 n_treatments = 4
-n_population = 13
+n_population = 10000
 
 # symptom names for easy reference
 from aux import symptom_names
 
 
 # Create the underlying population
+print("Generating population")
 population = simulator.Population(n_genes, n_vaccines, n_treatments)
 X = population.generate(n_population)
 
@@ -26,11 +27,40 @@ X = population.generate(n_population)
 print("Vaccination")
 vaccine_policy = policy.RandomPolicy(n_vaccines, list(range(-1,n_vaccines))) # make sure to add -1 for 'no vaccine'
 
+
+print("With a for loop")
+# The simplest way to work is to go through every individual in the population
+for t in range(n_population):
+    a_t = vaccine_policy.get_action(X[t])
+    # Then you can obtain results for everybody
+    y_t = population.vaccinate([t], a_t)
+    # Feed the results back in your policy
+    vaccine_policy.observe(X[t], a_t, y_t)
+
+print("Vaccinate'em all")
+# Here you can get an action for everybody in the population
 A = vaccine_policy.get_action(X)
+# Then you can obtain results for everybody
 Y = population.vaccinate(list(range(n_population)), A)
+# Feed the results back in your policy. 
 vaccine_policy.observe(X, A, Y)
 
-# Generate treatment results
+
+
+# You can do the same iteratively, where you select some to treat, and the rest remain untreated. The following implements a 3-phase vaccination policy.
+print("3-phase policy")
+for k in range(3):
+    A = vaccine_policy.get_action(X)
+    # Then you can obtain results for everybody
+    Y = population.vaccinate(list(range(n_population)), A)
+    # Feed the results back in your policy
+    vaccine_policy.observe(X, A, Y)
+
+
+
+    
+
+# We can do the same for treatments
 print("Treatment")
 treatment_policy = policy.RandomPolicy(n_treatments, list(range(n_treatments)))
 
